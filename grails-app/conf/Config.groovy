00ae3d6b228a541fa1eea2e89ad57d2b775ba4d2
Mary <maryam.beydokhti2005@gmail.com>
2012-12-23 16:41:55 +0330

    domain class alfa

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -91,3 +91,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'bahman.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'bahman.UserRole'
+grails.plugins.springsecurity.authority.className = 'bahman.Role'
